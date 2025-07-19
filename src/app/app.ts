9820f1bf399f9887232d57c1814a7bf9706791e6
redalgen <WhiteComment@protonmail.com>
2025-07-19 03:19:08 +0000

    feat: Setup Tailwind CSS
    Adds postcss config and tailwind dependencies. Also removes old app.css.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,9 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  selector: "app-root",
+  templateUrl: "./app.html",
 })
 export class App {
-  protected readonly title = signal('JustTheVideo');
+  protected readonly title = signal("JustTheVideo");
 }
